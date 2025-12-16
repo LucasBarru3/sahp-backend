7d44f518b9edcf1f68fee88d4cbe143003f04532
LucasBarru3 <lucasbarru3@gmail.com>
2025-12-16 18:38:15 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: '87.235.119.243',      // TU IP PÚBLICA
-  user: 'apiuser',
-  password: 'tucontraseña',
-  database: 'sahp_driving',
+  host: 'sql.freedb.tech',      // TU IP PÚBLICA
+  user: 'freedb_apiuser',
+  password: 'b%%y!fm3FnxC?8M',
+  database: 'freedb_sahp_driving',
   port: 3306,
   waitForConnections: true,
   connectionLimit: 5
 });
 
 module.exports = pool;
+
 
