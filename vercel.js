7afe8f8eb759495dcbaca2fb6991ea8d85bcbe4e
Rolk254 <rolk254@gmail.com>
2026-01-06 05:49:50 +0100

    test

--- vercel.js
+++ vercel.js
@@ -2,9 +2,9 @@
 export default {
   version: 2,
   builds: [
-    { src: 'api/*.js', use: '@vercel/node' }
+    { src: "api/*.js", use: "@vercel/node" }
   ],
   routes: [
-    { src: '/api/(.*)', dest: '/api/$1' }
+    { src: "/api/(.*)", dest: "/api/$1" }
   ]
 };
