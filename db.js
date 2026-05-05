f47d6a001db0dd138004d86e38c5d754e0080efc
Rolk254 <rolk254@gmail.com>
2026-05-05 16:09:06 +0200

    test database

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'sql306.infinityfree.com',      // TU IP PÚBLICA
-  user: 'if0_41539582',
-  password: 'Cj2EiU8Saiiljj',
-  database: 'if0_41539582_sahp',
+  host: 'sql104.iceiy.com',      // TU IP PÚBLICA
+  user: 'icei_41837816',
+  password: 'gonzalop',
+  database: 'icei_41837816_sahp',
   port: 3306,
   waitForConnections: true,
   connectionLimit: 5
