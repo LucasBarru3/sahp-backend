cec088ed6edf76489cd58371f3dbfe455298232b
Rolk254 <rolk254@gmail.com>
2026-05-15 00:12:09 +0200

    ultimo fix

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: '62.174.209.244',      // TU IP PÚBLICA
   user: 'sahp_user',
   password: 'sahp_pass',
-  database: 'sahp_backup',
+  database: 'sahp_backcup',
   port: 3306,
   waitForConnections: true,
   connectionLimit: 5
