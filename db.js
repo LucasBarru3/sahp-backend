f332a1802023dabaf2029695a2d7cc7becc8256a
Rolk254 <rolk254@gmail.com>
2026-05-15 14:11:12 +0200

    test

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: '62.174.209.244',      // TU IP PÚBLICA
-  user: 'sahp_user',
+  user: 'test',
   password: 'sahp_pass',
   database: 'sahp_backcup',
   port: 3306,
